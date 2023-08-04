38cf0189be61ba9d4a1a087b9726b6e089e9f6fd
NikitaShlapak <nikitashlapak04@gmail.com>
2023-08-04 17:56:00 +0300

    depletion setup

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from math import sqrt
 import numpy as np
 import openmc
 from openmc import deplete
+from openmc import stats
 
 from models.utils.params import GeometryParams, SourceParams
 
@@ -27,7 +28,6 @@ if __name__ == "__main__":
     colors = {water_mat: (120, 120, 255), cladding_mat: 'black', fuel_mat: (0, 200, 0), fuel_with_Gd_mat: (0, 100, 0),
               absorber_mat: (250, 125, 125)}
     color_data = dict(color_by='material', colors=colors)
-    # width = (params.TVS_edge_length * 5.1, params.TVS_edge_length * 5.1)
     width = np.array([params.TVS_edge_length * 5.1, params.TVS_edge_length * 5.1, ])
     scale = 5.1 / 2
     fig, ax = plt.subplots(2, 2)
@@ -44,7 +44,6 @@ if __name__ == "__main__":
     # ...and by openmc.Plots
     plots = [openmc.Plot(), openmc.Plot(), openmc.Plot(), openmc.Plot(), ]
     for i in range(4):
-        # plots[i].id=(i+1)*111
         plots[i].width = width
         plots[i].pixels = (500, 500)
         plots[i].basis = 'xz'
@@ -58,15 +57,15 @@ if __name__ == "__main__":
 
     # Settings
     setting = openmc.Settings()
-    setting.batches = 50
-    setting.inactive = 10
+    setting.batches = 100
+    setting.inactive = 20
     setting.particles = 10000
     setting.run_mode = 'fixed source'
 
     source = openmc.Source()
-    source.space = openmc.stats.Box([-100, -100, -350 / 2], [100, 100, 350 / 2], only_fissionable=True)
-    n = SourceParams.neutrons_per_second
-    source.time = openmc.stats.Uniform(n, 1)
+    source.space = stats.Box([-100, -100, -350 / 2], [100, 100, 350 / 2], only_fissionable=True)
+    # n = SourceParams.neutrons_per_second
+    # source.time = stats.Uniform(n, 1)
     setting.source = source
 
     # Tallies
@@ -78,7 +77,6 @@ if __name__ == "__main__":
     U_tally.nuclides = ['U235', 'U238', 'Gd152', 'O16', 'H1']
 
     tallies = openmc.Tallies([U_tally, flux_tally])
-    # tally.filters = [cell_filter,energy_filter]
 
     # XML export
     materials.export_to_xml('data/xmls/materials.xml')
@@ -86,32 +84,26 @@ if __name__ == "__main__":
     setting.export_to_xml('data/xmls/settings.xml')
     tallies.export_to_xml('data/xmls/tallies.xml')
     plots.export_to_xml('data/xmls/plots.xml')
-    # materials.export_to_xml()
-    # geometry.export_to_xml()
-    # setting.export_to_xml()
-    # tallies.export_to_xml()
-    # plots.export_to_xml()
+
 
     # RUN
     model = openmc.Model(geometry=geometry, materials=materials, tallies=tallies, plots=plots, settings=setting)
-    # print(setting.run_mode)
     # openmc.plot_geometry(path_input='data/xmls/', output=False)
     openmc.run(output=True, path_input='data/xmls/')
 
     # Deplition
-    # operator = deplete.CoupledOperator(model=model)
-    # power = 1000e6/163*7
-    # time_steps = [1]
-    # integrator = deplete.PredictorIntegrator(operator, time_steps, power, timestep_units='s')
-    # integrator
-    # integrator.integrate()
+    operator = deplete.CoupledOperator(model=model)
+    power = 1000e6/163*7
+    time_steps = [1]
+    integrator = deplete.PredictorIntegrator(operator, time_steps, power, timestep_units='s')
+    integrator.integrate()
 
-    with openmc.StatePoint('statepoint.100.h5') as sp:
-        print(sp.keff)
-        output_tally = sp.get_tally(name='fuel')
-        df = output_tally.get_pandas_dataframe()
-        # df.to_csv('out_fuel_3.6.csv')
-        output_tally = sp.get_tally(name='flux')
-        flux = output_tally.get_pandas_dataframe()
-        # flux.to_csv('out_flux_3.6.csv')
-        print(df, flux, sep='\n\n')
+    # with openmc.StatePoint('statepoint.100.h5') as sp:
+    #     print(sp.keff)
+    #     output_tally = sp.get_tally(name='fuel')
+    #     df = output_tally.get_pandas_dataframe()
+    #     # df.to_csv('out_fuel_3.6.csv')
+    #     output_tally = sp.get_tally(name='flux')
+    #     flux = output_tally.get_pandas_dataframe()
+    #     # flux.to_csv('out_flux_3.6.csv')
+    #     print(df, flux, sep='\n\n')
